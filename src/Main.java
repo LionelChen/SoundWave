11905197140684b23f04264e33da663972a7410d
Linchuan Chen <lionelchen@outlook.com>
2022-02-10 22:18:15 +0800

    support both mono and stereo, cleaning code

--- src/Main.java
+++ src/Main.java
@@ -28,68 +28,19 @@ public class Main {
         //
         String[] pamss = new String[] { "-r", "-g", "-b" };
         if (reader.isSuccess()) {
-
-/*            PlotFrame frame = Plot.figrue(String.format("%s %dHZ %dBit %dCH", filename, reader.getSampleRate(), reader.getBitPerSample(), reader.getNumChannels()));
-            frame.setSize(500, 200);
-            Plot.hold_on();
-
-            for (int i = 0; i < reader.getNumChannels(); ++i) {
-                // 获取i声道数据
-                int[] data = reader.getData()[i];
-                // 绘图
-                Plot.plot(Integers2Doubles(data), pamss[i % pamss.length]);
-            }
-            Plot.hold_off();*/
-
             int[][] raw_data = reader.getData();
             int[] data = reader.getData()[0];
-/*            XYLineChart_AWT chart1 = new XYLineChart_AWT(
-                    filename + "; Sample Rate: " +reader.getSampleRate() + "; BitsPerSample: " + reader.getBitPerSample() + "; # of Channel: " +reader.getNumChannels(),
-                    "Waveform", raw_data[0]);
-
-            chart1.pack();
-            RefineryUtilities.centerFrameOnScreen( chart1 );
-            chart1.setVisible( true );*/
 
             JFrame frame = new JFrame( filename + "; Sample Rate: " +reader.getSampleRate() + "; BitsPerSample: " + reader.getBitPerSample() + "Waveform");
             frame.setLayout( new FlowLayout() );
 
-            final XYSeriesCollection dataset0 = new XYSeriesCollection();
-            final XYSeries channel_0 = new XYSeries( "Channel_0" );
-            for(int i = 0; i<raw_data[0].length; i++){
-                channel_0.add(i, raw_data[0][i]);
+            frame.getContentPane().add(processingRawData(raw_data[0], "Channel 0"));
+
+            if(reader.getNumChannels()==2){
+                frame.getContentPane().add(processingRawData(raw_data[1], "Channel 1"));
             }
-            dataset0.addSeries( channel_0 );
-
-            JFreeChart xyChart1 =
-                    ChartFactory.createXYLineChart("Channel_0","", "", dataset0,
-                            PlotOrientation.VERTICAL, true, true, false);
-
-            ChartPanel chartPanel1 = new ChartPanel( xyChart1 );
-            chartPanel1.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
-
-            frame.getContentPane().add(chartPanel1);
-
-
-            final XYSeriesCollection dataset1 = new XYSeriesCollection();
-            final XYSeries channel_1 = new XYSeries( "Channel_0" );
-            for(int i = 0; i<raw_data[1].length; i++){
-                channel_1.add(i, raw_data[1][i]);
-            }
-            dataset1.addSeries( channel_1 );
-
-            JFreeChart xyChart2 =
-                    ChartFactory.createXYLineChart("Channel_1","", "", dataset1,
-                            PlotOrientation.VERTICAL, true, true, false);
-
-            ChartPanel chartPanel2 = new ChartPanel( xyChart2 );
-            chartPanel2.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
-
-            frame.getContentPane().add(chartPanel2);
-
 
             frame.pack();
-
             frame.setVisible(true);
 
 
@@ -97,6 +48,24 @@ public class Main {
         } else {
             System.err.println(filename + " might not be a valid wav file");
         }
+    }
+
+
+    public static ChartPanel processingRawData(int[] data, String channelNumber){
+        final XYSeriesCollection dataset0 = new XYSeriesCollection();
+        final XYSeries channel_0 = new XYSeries( channelNumber );
+        for(int i = 0; i<data.length; i++){
+            channel_0.add(i, data[i]);
+        }
+        dataset0.addSeries( channel_0 );
+
+        JFreeChart xyChart1 =
+                ChartFactory.createXYLineChart(channelNumber,"", "", dataset0,
+                        PlotOrientation.VERTICAL, true, true, false);
+
+        ChartPanel chartPanel1 = new ChartPanel( xyChart1 );
+        chartPanel1.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
+        return chartPanel1;
     }
 
     public static void main(String[] args) {
